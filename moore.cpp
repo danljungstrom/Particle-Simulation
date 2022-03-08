a22c37ffe8fec9ad9a7e90ec277d6492b4c05a21
Daniel Nordin <daniel.nordin14@gmail.com>
2022-03-08 12:04:44 +0100

    grid_size done

--- moore.cpp
+++ moore.cpp
@@ -2,18 +2,37 @@
 #include <stdlib.h>
 #include <list>
 #include <vector>
+#include "common.cpp"
 
-typedef std::list<int> Int_list;
-typedef std::vector<std::vector<Int_list>> Nh;
+using namespace std;
+
+typedef list<particle_t*> Part_list;
+typedef vector<Part_list> Nh;
 
 void* create_neighbourhood(int n){
-    Nh nh(n);
+    set_size(n);
+    int rows = (int) (size/cutoff + 1);
+    int grid_size = rows * rows + rows;
+    Nh nh(grid_size);
     void* ptr = malloc(sizeof(nh));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            Int_list l = {};
-            nh[i][j] = l;
-        }
+    for(int i = rows; i < grid_size; i++){
+            Part_list l = {};
+            nh[i] = l;
     }
     return ptr;
+}
+
+void add_particle(particle_t* particle, Nh &nh){
+    int x = ((int)particle->ax) / cutoff;
+    int y = ((int)particle->ay) / cutoff;
+    int cord = x + y;
+    Nh n = nh;
+
+    n[cord].push_back(particle);
+}
+
+void remove_particle(particle_t* particle, int x, int y, Nh &nh){
+    Nh n = nh;
+
+    n[x].remove(particle);
 }
